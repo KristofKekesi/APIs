6c5aab3a6b9138250737bd3d21a72bef3ef01f13
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-09-15 22:55:31 +0200

    upd

---
+++ Preacher/v1/lang/hu-HU/MagyarBibliatársulatKároli2014/ÓSzövetség/1Móz1/1Móz1_1/index.js
@@ -0,0 +1,132 @@
+var info = [
+	{
+		"type": "title&num",
+		"value": "A világ teremtése",
+	}, {
+		"type": "line",
+		"value": "Kezdetben teremté Isten az eget és a földet.",
+	}, {
+		"type": "line",
+		"value": "A föld pedig kietlen és puszta vala, és setétség vala a mélység színén, és az Isten Lelke lebeg vala a vizek felett.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Legyen világosság: és lőn világosság.",
+	}, {
+		"type": "line",
+		"value": "És  látá Isten, hogy jó a világosság; és elválasztá Isten a világosságot a setétségtől.",
+	}, {
+		"type": "line",
+		"value": "És nevezé Isten a világosságot nappalnak, és a setétséget nevezé éjszakának: és lőn este és lőn reggel, első nap.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Legyen mennyezet a víz között, amely elválassza a vizeket a vizektől.",
+	}, {
+		"type": "line",
+		"value": "Teremté tehát Isten a mennyezetet, és elválasztá a mennyezet alatt való vizeket, a mennyezet felett való vizektől. És úgy lőn.",
+	}, {
+		"type": "line",
+		"value": "És nevezé Isten a mennyezetet égnek: és lőn este, és lőn reggel, második nap.",
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Gyűljenek egybe az ég alatt való vizek egy helyre, hogy tessék meg a száraz. És úgy lőn.",
+	}, {
+		"type": "line",
+		"value": "És nevezé Isten a szárazat földnek; az egybegyűlt vizeket pedig tengernek nevezé. És látá Isten, hogy jó.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "Azután monda Isten: Hajtson a föld gyenge füvet, maghozó füvet, gyümölcsfát, amely gyümölcsöt hozzon az ő neme szerint, amelyben legyen néki magva e földön. És úgy lőn.",
+	}, {
+		"type": "line",
+		"value": "Hajta tehát a föld gyenge füvet, maghozó füvet az ő neme szerint, és gyümölcstermő fát, amelynek gyümölcsében mag van az ő neme szerint. És látá Isten, hogy jó.",
+	}, {
+		"type": "line",
+		"value": "És lőn este és lőn reggel, harmadik nap.",
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Legyenek világító testek az ég mennyezetén, hogy elválasszák a nappalt az éjszakától, és legyenek jelek, és meghatározói ünnepeknek, napoknak és esztendőknek.",
+	}, {
+		"type": "line",
+		"value": "És legyenek világítókul az ég mennyezetén hogy világítsanak a földre. És úgy lőn.",
+	}, {
+		"type": "line",
+		"value": "Teremté tehát Isten a két nagy világító testet: a nagyobbik világító testet, hogy uralkodjék nappal és a kisebbik világító testet, hogy uralkodjék éjjel; és a csillagokat.",
+	}, {
+		"type": "line",
+		"value": "És helyezteté Isten azokat az ég mennyezetére, hogy világítsanak a földre;",
+	}, {
+		"type": "line",
+		"value": "És hogy uralkodjanak a nappalon és az éjszakán, és elválasszák a világosságot a setétségtől. És látá Isten, hogy jó.",
+	}, {
+		"type": "line",
+		"value": "És lőn este és lőn reggel, negyedik nap.",
+	}, {
+		"type": "sub",
+		"value": "Az állatok teremtése",
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Pezsdűljenek a vizek élő állatok nyüzsgésétől; és madarak repdessenek a föld felett, az ég mennyezetének színén.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És teremté Isten a nagy vízi állatokat, és mindazokat a csúszó-mászó állatokat, amelyek nyüzsögnek a vizekben az ő nemök szerint, és mindenféle szárnyas repdesőt az ő neme szerint. És látá Isten, hogy jó.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És megáldá azokat Isten, mondván: Szaporodjatok, és sokasodjatok, és töltsétek be a tenger vizeit; a madár is sokasodjék a földön.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És lőn este és lőn reggel, ötödik nap.",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És lőn este és lőn reggel, ötödik nap.",
+	}, {
+		"type": "line",
+		"value": "Azután monda az Isten: Hozzon a föld élő állatokat nemök szerint: barmokat, csúszó-mászó állatokat és szárazföldi vadakat nemök szerint. És úgy lőn.\n",
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "Teremté tehát Isten a szárazföldi vadakat nemök szerint, a barmokat nemök szerint, és a földön csúszó-mászó mindenféle állatokat nemök szerint. És látá Isten, hogy jó.",
+	}, {
+		"type": "sub",
+		"value": "Az ember teremtése"
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Teremtsünk embert a mi képünkre és hasonlatosságunkra; és uralkodjék a tenger halain, az ég madarain, a barmokon, mind az egész földön, és a földön csúszó-mászó mindenféle állatokon."
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "Teremté tehát az Isten az embert az ő képére, Isten képére teremté őt: férfiúvá és asszonynyá teremté őket."
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És megáldá Isten őket, és monda nékik Isten: Szaporodjatok és sokasodjatok, és töltsétek be a földet és hajtsátok birodalmatok alá; és uralkodjatok a tenger halain, az ég madarain, és a földön csúszó-mászó mindenféle állatokon."
+	}, {
+		"type": "break"
+	}, {
+		"type": "line",
+		"value": "És monda Isten: Ímé néktek adok minden maghozó füvet az egész föld színén, és minden fát, amelyen maghozó gyümölcs van; az legyen néktek eledelül."
+	}, {
+		"type": "line",
+		"value": "A föld minden vadainak pedig, és az ég minden madarainak, és a földön csúszó-mászó mindenféle állatoknak, amelyekben élő lélek van, a zöld füveket adom eledelül. És úgy lőn."
+	}, {
+		"type": "line",
+		"value": "És látá Isten, hogy minden amit teremtett vala, ímé igen jó. És lőn este és lőn reggel, hatodik nap."
+	}
+]
+
+document.getElementsByTagName("body")[0].innerHTML = JSON.stringify(info);
