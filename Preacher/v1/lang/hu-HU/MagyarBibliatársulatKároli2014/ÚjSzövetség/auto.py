7e047698d2739d07d6a7af0e736e1242c727011a
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-11-28 02:18:31 +0100

    ⌨️ Reworked API

--- Preacher/v1/lang/hu-HU/MagyarBibliatársulatKároli2014/ÚjSzövetség/auto.py
+++
@@ -1,7 +0,0 @@
-import os
-
-dirName = input("könyv rendszeri neve: ")
-externalNum = input("fejezetek száma: ")
-
-for num in range(int(externalNum)):
-    os.system("mkdir " + dirName + "_" + str(num+1))
