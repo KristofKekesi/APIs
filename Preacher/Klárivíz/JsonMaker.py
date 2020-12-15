cc417295458d122e8ab85d451a0909e597e6e769
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-12-15 15:44:46 +0100

    upd

--- Preacher/Klárivíz/JsonMaker.py
+++ Preacher/Klárivíz/JsonMaker.py
@@ -7,61 +7,67 @@ pasteFile = "pasteFile.txt"
 finalFolder = "final"
 
 while True:
-    create = codecs.open(pasteFile, "w", "UTF-8")
-    create.close()
-    os.system('cls')
-    while True:
-        print('Másold be az pasteFile.txt-be, majd nyomd meg az entert')
-        input()
-        break
-    while True:
-        with codecs.open(pasteFile, "r", "UTF-8") as f:
-            if len(f.read()) == 0:
-                os.system('cls')
-                input('Üres fájl, továbblépéshez enter.')
-            else:
-                while True:
-                    os.system('cls')
-                    print("Írja be a könyvet (például 1Móz1)")
-                    book = input("")
-                    print("Írja be fejezet számát (például 1)")
-                    number = input("")
-                    os.system('mkdir ' + finalFolder)
+	create = codecs.open(pasteFile, "w", "UTF-8")
+	create.close()
+	os.system('cls')
+	while True:
+		print('Másold be az pasteFile.txt-be, majd nyomd meg az entert')
+		input()
+		break
+	while True:
+		with codecs.open(pasteFile, "r", "UTF-8") as f:
+			if len(f.read()) == 0:
+				os.system('cls')
+				input('Üres fájl, továbblépéshez enter.')
+			else:
+				while True:
+					os.system('cls')
+					print("Írja be a könyvet (például 1Móz1)")
+					book = input("")
+					if book != "":
+						break
+				while True:
+					print("Írja be fejezet számát (például 1)")
+					number = input("")
+					if number != "":
+						break
+				while True:
+					os.system('mkdir ' + finalFolder)
 
-                    object = [
-                        '{',
-                        '   "meta": {',
-                        '   "type": "text",',
-                        '   "name": "' + number + '. Fejezet"',
-                        '   },',
-                        '   "data": [',
-                    ]
+					object = [
+						'{',
+						'   "meta": {',
+						'   "type": "text",',
+						'   "name": "' + number + '. Fejezet"',
+						'   },',
+						'   "data": [',
+					]
 
-                    read = codecs.open(pasteFile, "r", "UTF-8")
-                    for lines in read:
-                        lines = lines.strip().replace("\n", "").replace("", "-").replace("\"", "\\\"")
-                        try:
-                            num = int(lines)
-                        except:
-                            object.append('        {')
-                            object.append('            "type": "line",')
-                            object.append('            "value": "' + lines + '"')
-                            object.append('        },')
-                    object.pop()
-                    object.append('        }')
-                    object.append('    ]')
-                    object.append('}')
-                    read.close()
+					read = codecs.open(pasteFile, "r", "UTF-8")
+					for lines in read:
+						lines = lines.strip().replace("\n", "").replace("", "-").replace("\"", "\\\"")
+						try:
+							num = int(lines)
+						except:
+							object.append('        {')
+							object.append('            "type": "line",')
+							object.append('            "value": "' + lines + '"')
+							object.append('        },')
+					object.pop()
+					object.append('        }')
+					object.append('    ]')
+					object.append('}')
+					read.close()
 
-                    path = "final/" + book + "_" + number + "/"
+					path = "final/" + book + "_" + number + "/"
 
-                    os.system("mkdir final\\" + book + "_" + number)
+					os.system("mkdir final\\" + book + "_" + number)
 
-                    read = codecs.open(path + "data.json", "w", "UTF-8")
-                    for element in object:
-                        print(element, file=read)
-                    read.close()
-                    os.system('cls')
-                    input("JSON Legenerálva (Továbblépéshez Enter)")
-                    break
-        break
+					read = codecs.open(path + "data.json", "w", "UTF-8")
+					for element in object:
+						print(element, file=read)
+					read.close()
+					os.system('cls')
+					input("JSON Legenerálva (Továbblépéshez Enter)")
+					break
+		break
